d35738ab9865d3188630d3b5d31a7c33a33b1a71
Tamás Rigó <rigo.tamas.seb@gmail.com>
2023-05-16 09:36:39 +0200

    add svelte and carousel component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://beridoor.hu",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), svelte()]
 });
---
+++ src/components/carousel.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Carousel from 'svelte-carousel'
+  export let images;
+
+  let carousel; // for calling methods of the carousel instance
+  const handleNextClick = () => {
+    carousel.goToNext()
+  }
+</script>
+
+<Carousel
+  autoplay
+  autoplayDuration={5000}
+  autoplayProgressVisible
+  pauseOnFocus
+  bind:this={carousel}
+>
+  {#each images as { src, alt }}
+        <img {src} {alt} />
+  {/each}
+</Carousel>
+
+<button on:click={handleNextClick}>Next</button>
