e06c9f6abfead40055d2b24e3d3bf875f69f92bd
Tamás Rigó <rigo.tamas.seb@gmail.com>
2023-07-05 20:27:12 +0200

    change hex value to beridoorBlue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      ...colors,
+      beridoorBlue: "#1f3864",
+      beridoorBlueDeeper: "#152645",
+    },
     extend: {
       fontFamily: {
-        sans: ["Dosis", "Inter", ...defaultTheme.fontFamily.sans],
-        dosis: ['Dosis', 'sans-serif']
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        dosis: ["Dosis", "sans-serif"],
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
